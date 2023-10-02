413b69902159f5406ce625140f8d78ec2453ea9a
Lama <nascimentofelipe565@gmail.com>
2023-10-02 17:27:37 -0300

    Added authentication

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,63 +1,78 @@
 import { X } from "@phosphor-icons/react";
 import * as Dialog from "@radix-ui/react-dialog";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import CardTopico from "../components/CardTopico";
 import CriarTopicoForm from "../components/CriarTopicoForm";
 import FazerTicket from "../components/FazerTicket";
+import UserProfile from "../components/UserProfile";
 import VerTickets from "../components/VerTickets";
+import { auth } from "../lib/firebase";
 
 export default function Home() {
   const navigate = useNavigate();
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  console.log(isAuthenticated);
+
+  auth.onAuthStateChanged((u) => {
+    u ? setIsAuthenticated(true) : setIsAuthenticated(false);
+  });
 
   return (
     <div className="grid grid-cols-3 gap-4 bg-primary w-screen h-fit text-white overflow-hidden">
       {/* Botoes do lado esquerdo */}
       <div>
         {/* Botao do form de Criar topico */}
-        <Dialog.Root>
-          <Dialog.Trigger
-            type="button"
-            className="bg-secondary text-white p-2 m-4 font-bold rounded-lg"
-          >
-            Criar topico
-          </Dialog.Trigger>
+        {isAuthenticated && (
+          <Dialog.Root>
+            <Dialog.Trigger
+              type="button"
+              className="bg-secondary text-white p-2 m-4 font-bold rounded-lg"
+            >
+              Criar topico
+            </Dialog.Trigger>
 
-          <Dialog.Portal>
-            <Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-0" />
-            <Dialog.Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-xl top-40 left-96 ml-72">
-              <Dialog.Close className="absolute right-6 top-6 text-zinc-400 rounded-lg hover:text-zinc-200 focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-zinc-900">
-                <X size={24} aria-label="Fechar" />
-              </Dialog.Close>
+            <Dialog.Portal>
+              <Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-0" />
+              <Dialog.Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-xl top-40 left-96 ml-72">
+                <Dialog.Close className="absolute right-6 top-6 text-zinc-400 rounded-lg hover:text-zinc-200 focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-zinc-900">
+                  <X size={24} aria-label="Fechar" />
+                </Dialog.Close>
 
-              <Dialog.Title className="text-white text-center font-bold text-xl">
-                Criar Topico
-              </Dialog.Title>
+                <Dialog.Title className="text-white text-center font-bold text-xl">
+                  Criar Topico
+                </Dialog.Title>
 
-              <CriarTopicoForm />
-            </Dialog.Content>
-          </Dialog.Portal>
-        </Dialog.Root>
+                <CriarTopicoForm />
+              </Dialog.Content>
+            </Dialog.Portal>
+          </Dialog.Root>
+        )}
 
         {/* Botao do form de Ver tickets */}
-        <Dialog.Root>
-          <Dialog.Trigger
-            type="button"
-            className="bg-secondary text-white p-2 m-4 font-bold rounded-lg"
-          >
-            Ver Tickets
-          </Dialog.Trigger>
+        {isAuthenticated && (
+          <Dialog.Root>
+            <Dialog.Trigger
+              type="button"
+              className="bg-secondary text-white p-2 m-4 font-bold rounded-lg"
+            >
+              Ver Tickets
+            </Dialog.Trigger>
 
-          <Dialog.Portal>
-            <Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-0" />
-            <Dialog.Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-xl top-40 left-96 ml-72">
-              <Dialog.Close className="absolute right-6 top-6 text-zinc-400 rounded-lg hover:text-zinc-200 focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-zinc-900">
-                <X size={24} aria-label="Fechar" />
-              </Dialog.Close>
+            <Dialog.Portal>
+              <Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-0" />
+              <Dialog.Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-xl top-40 left-96 ml-72">
+                <Dialog.Close className="absolute right-6 top-6 text-zinc-400 rounded-lg hover:text-zinc-200 focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-zinc-900">
+                  <X size={24} aria-label="Fechar" />
+                </Dialog.Close>
 
-              <VerTickets />
-            </Dialog.Content>
-          </Dialog.Portal>
-        </Dialog.Root>
+                <VerTickets />
+              </Dialog.Content>
+            </Dialog.Portal>
+          </Dialog.Root>
+        )}
 
         {/* Coluna da parte do meio */}
       </div>
@@ -91,41 +106,47 @@ export default function Home() {
             titulo="Como eu poderia aplicar melhor a teoria das cores 
           em meu projeto?"
           />
-
-          <CardTopico titulo="Porque Jorge Aragao eh o cara mais brabo do mundo?" />
         </div>
       </div>
+
       {/* Botoes do lado direito */}
-      <div className="flex flex-row items-start justify-end">
-        <Dialog.Root>
-          <Dialog.Trigger
-            type="button"
-            className="bg-secondary text-white p-2 m-4 font-bold rounded-lg"
+      <div className="flex flex-row items-start justify-center">
+        {isAuthenticated && (
+          <Dialog.Root>
+            <Dialog.Trigger
+              type="button"
+              className="bg-secondary text-white p-2 m-4 font-bold rounded-lg"
+            >
+              Fazer ticket
+            </Dialog.Trigger>
+
+            <Dialog.Portal>
+              <Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-0" />
+              <Dialog.Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-xl top-40 left-96 ml-72">
+                <Dialog.Close className="absolute right-6 top-6 text-zinc-400 rounded-lg hover:text-zinc-200 focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-zinc-900">
+                  <X size={24} aria-label="Fechar" />
+                </Dialog.Close>
+
+                <Dialog.Title className="text-white text-center font-bold text-xl">
+                  Fazer ticket
+                </Dialog.Title>
+
+                <FazerTicket />
+              </Dialog.Content>
+            </Dialog.Portal>
+          </Dialog.Root>
+        )}
+
+        {isAuthenticated ? (
+          <UserProfile />
+        ) : (
+          <button
+            className="text-white w-fit m-4 bg-secondary p-2 rounded-lg font-bold"
+            onClick={() => navigate("/login")}
           >
-            Fazer ticket
-          </Dialog.Trigger>
-
-          <Dialog.Portal>
-            <Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-0" />
-            <Dialog.Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-xl top-40 left-96 ml-72">
-              <Dialog.Close className="absolute right-6 top-6 text-zinc-400 rounded-lg hover:text-zinc-200 focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-zinc-900">
-                <X size={24} aria-label="Fechar" />
-              </Dialog.Close>
-
-              <Dialog.Title className="text-white text-center font-bold text-xl">
-                Fazer ticket
-              </Dialog.Title>
-
-              <FazerTicket />
-            </Dialog.Content>
-          </Dialog.Portal>
-        </Dialog.Root>
-        <button
-          className="text-white w-fit m-4 bg-secondary p-2 rounded-lg font-bold"
-          onClick={() => navigate("/login")}
-        >
-          Login / Signup
-        </button>
+            Login / Signup
+          </button>
+        )}
       </div>
     </div>
   );
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,92 +1,112 @@
 import { GoogleLogo } from "@phosphor-icons/react";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { FormEvent, useState } from "react";
+import { UserCredential, signInWithEmailAndPassword } from "firebase/auth";
+import { FormEvent, createContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../lib/firebase";
+
+const UserContext = createContext<UserCredential | undefined>(undefined);
 
 export default function Login() {
   const navigate = useNavigate();
 
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
+  const [user, setUser] = useState<UserCredential>();
 
   async function handleLogin(event: FormEvent) {
     event.preventDefault();
 
-    const user = await signInWithEmailAndPassword(auth, email, senha);
+    try {
+      const tryUser = await signInWithEmailAndPassword(auth, email, senha);
 
-    console.log(user);
+      setUser(tryUser);
+
+      console.log(user);
+
+      history.pushState(user, "user");
+      navigate("/");
+    } catch (error) {}
   }
 
   return (
-    <div className="bg-primary w-screen h-screen text-white flex flex-col items-center justify-center">
-      <div className="bg-secondary rounded-lg flex flex-col p-10">
-        <h1
-          className="text-6xl font-bold"
-          style={{ cursor: "pointer" }}
-          onClick={() => {
-            navigate("/");
-          }}
-        >
-          DevDen
-        </h1>
+    <UserContext.Provider value={user}>
+      <div className="bg-primary w-screen h-screen text-white flex flex-col items-center justify-center">
+        <div className="bg-secondary rounded-lg flex flex-col p-10">
+          <h1
+            className="text-6xl font-bold"
+            style={{ cursor: "pointer" }}
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            DevDen
+          </h1>
 
-        <h2 className="text-3xl mt-10 font-bold">Login</h2>
+          <h2 className="text-3xl mt-10 font-bold">Login</h2>
 
-        <label htmlFor="email" className="mt-10">
-          Email
-        </label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          className="p-2 bg-terciary rounded-lg"
-          value={email}
-          onChange={(event) => {
-            setEmail(event.target.value);
-          }}
-        />
+          <label htmlFor="email" className="mt-10">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="p-2 bg-terciary rounded-lg"
+            value={email}
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+          />
 
-        <label htmlFor="senha" className="mt-10">
-          Senha
-        </label>
-        <input
-          type="password"
-          name="senha"
-          id="senha"
-          className="p-2 bg-terciary rounded-lg"
-          value={senha}
-          onChange={(event) => {
-            setSenha(event.target.value);
-          }}
-        />
+          <label htmlFor="senha" className="mt-10">
+            Senha
+          </label>
+          <input
+            type="password"
+            name="senha"
+            id="senha"
+            className="p-2 bg-terciary rounded-lg"
+            value={senha}
+            onChange={(event) => {
+              setSenha(event.target.value);
+            }}
+          />
 
-        <button
-          className="mt-10 p-4 bg-terciary rounded-lg font-bold"
-          onClick={handleLogin}
-        >
-          Entrar
-        </button>
+          <button
+            className="mt-10 p-4 bg-terciary rounded-lg font-bold"
+            onClick={handleLogin}
+            onKeyDown={(key) => {
+              if (key.code === "Enter") {
+                handleLogin;
+              }
+            }}
+          >
+            Entrar
+          </button>
 
-        <button className="mt-10 p-4 w-64 bg-terciary rounded-3xl flex items-center justify-around">
-          <GoogleLogo /> Continuar com o Google
-        </button>
+          <div className="flex flex-col gap-4 text-center">
+            <button className="mt-10 p-4 w-64 bg-terciary rounded-3xl flex items-center justify-around">
+              <GoogleLogo /> Continuar com o Google
+            </button>
 
-        <div className="flex justify-center items-center">
-          <p>
-            Primeira vez aqui?{" "}
-            <span
-              className="text-blue-600 mt-4"
-              onClick={() => {
-                navigate("/signup");
-              }}
-              style={{ cursor: "pointer" }}
-            >
-              Cadastre-se
-            </span>
-          </p>
+            <div className="flex justify-center items-center">
+              <p>
+                Primeira vez aqui?
+                <br />
+                <span
+                  className="text-blue-600"
+                  onClick={() => {
+                    navigate("/signup");
+                  }}
+                  style={{ cursor: "pointer" }}
+                >
+                  Cadastre-se
+                </span>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </UserContext.Provider>
   );
 }
